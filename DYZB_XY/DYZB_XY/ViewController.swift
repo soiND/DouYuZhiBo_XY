ef2ac58655dd92ffa1d6760b9f4cefa9cf36293d
xuyu <yuxua@isoftstone.com>
2016-09-20 16:08:33 +0800

    初始化项目

---
+++ DYZB_XY/DYZB_XY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYZB_XY
+//
+//  Created by xuyu on 16/9/20.
+//  Copyright © 2016年 xuyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
